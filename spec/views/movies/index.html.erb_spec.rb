ada1c550b8e9039da0d4c09f909a156c08a3f9a3
Maor Leger <maor.leger@gmail.com>
2016-05-15 17:40:24 -0600

    basic movie view

--- spec/views/movies/index.html.erb_spec.rb
+++ spec/views/movies/index.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "movies/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @movie1 = build_stubbed(:movie)
+    @movie2 = build_stubbed(:movie)
+    assign(:movies, [ @movie1, @movie2 ])
+  end
+  it "displays all the movies" do
+    render
+
+    expect(rendered).to include(@movie1.title, @movie2.title)
+  end
 end
