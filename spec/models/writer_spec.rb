b94ac8ea2bd598ffe0041a0e75013264fc5c61d6
Maor Leger <maor.leger@gmail.com>
2016-05-14 18:37:32 -0600

    added breaking test for next time

--- spec/models/writer_spec.rb
+++ spec/models/writer_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Writer, type: :model do
   it 'has a valid factory' do
     expect(build_stubbed(:writer)).to be_valid
   end
+
+  it 'can join to movies' do
+    movie = build_stubbed(:movie)
+    expect(create(:writer, movies: [movie])).to eq([movie])
+  end
 end
