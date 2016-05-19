e010e2822713faa4db3420f83ab4bba24e49b944
Maor Leger <maor.leger@gmail.com>
2016-05-18 19:45:33 -0600

    few random changes

--- spec/views/movies/index.html.erb_spec.rb
+++ spec/views/movies/index.html.erb_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'movies/index.html.erb', type: :view do
-  before do
-  end
   it 'displays all the movies' do
-    @movie1 = build_stubbed(:movie)
-    @movie2 = build_stubbed(:movie)
+    @movie1 = create(:movie)
+    @movie2 = create(:movie)
     assign(:movies, [@movie1, @movie2])
 
     render
