3e9a19ba2f5949043ca67fa81b19c2a8895b4865
Maor Leger <maor.leger@gmail.com>
2016-05-20 18:43:07 -0600

    datatables

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    $("#movies").DataTable({
+
+    });
     $(".show-more").click(function(e) {
         var id = $(this).data('id');
         $("#movie-details-" + id).toggle('fast')
