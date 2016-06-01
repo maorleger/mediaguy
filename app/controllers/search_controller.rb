1f357b36f9ab97625d51ad4e68d0522ec4ee28d6
Maor Leger <maor.leger@gmail.com>
2016-05-31 21:57:31 -0600

    added support for searching canistreamit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,24 @@
 class SearchController < ApplicationController
   def search
     search_param = params[:name]
-    @results = []
-    local_movies = Movie.where('title like ?', "%#{search_param}%").to_a
-    @results += local_movies if local_movies
+    include_streaming = params[:include_streaming]
+    @results = results(search_param, include_streaming)
+  end
+
+  private
+
+  def results(search_text, include_streaming)
+    results = local_results(search_text)
+    results += canistreamit_results(search_text) if include_streaming
+    results
+  end
+
+  def local_results(search_text)
+    local_movies = Movie.where('title like ?', "%#{search_text}%")
+    local_movies.any? ? local_movies.to_a : []
+  end
+
+  def canistreamit_results(search_text)
+    Canistreamit::Client.new.search_and_query(search_text)
   end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe SearchController, type: :controller do
   describe 'GET #search' do
     before do
       @movie = create(:movie)
-      @movie_prefix = create(:movie, title: 'movie 1')
-      @movie_prefix2 = create(:movie, title: 'movie 2')
+      @movie_prefix = create(:movie, title: 'maor 1')
+      @movie_prefix2 = create(:movie, title: 'maor 2')
     end
 
     it 'returns http success' do
@@ -20,7 +20,7 @@ RSpec.describe SearchController, type: :controller do
     end
 
     it 'can return multiple movies by prefix' do
-      get :search, name: 'movie'
+      get :search, name: 'maor'
       expect(assigns(:results)).not_to be_nil
       expect(assigns(:results)).to eq([@movie_prefix, @movie_prefix2])
     end
@@ -28,6 +28,12 @@ RSpec.describe SearchController, type: :controller do
     it 'returns an empty array if no movies are found' do
       get :search, name: 'fuzzywazzywashe'
       expect(assigns[:results]).to eq([])
+    end
+
+    it 'can search canistreamit' do
+      get :search, name: 'goodfellas', include_streaming: true
+      # TODO: fix this test once i decide on an interface for canistreamit
+      expect(assigns[:results][0]['movie']).to include('title' => 'GoodFellas')
     end
   end
 end
