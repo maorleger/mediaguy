871a536addeb184f2394d53d338e9b9a25c175e8
Maor Leger <maor.leger@gmail.com>
2016-06-04 15:15:22 -0600

    worked on the movie search interface

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,49 @@
 class SearchController < ApplicationController
   def search
-    search_param = params[:name]
+    search_param = params[:name] || ''
     include_streaming = params[:include_streaming]
+    @client = Canistreamit::Client.new
     @results = results(search_param, include_streaming)
+  end
+
+  def default_results_hash
+    {
+      movie: {},
+      local: [],
+      streaming: []
+    }
   end
 
   private
 
   def results(search_text, include_streaming)
-    results = local_results(search_text)
-    results += canistreamit_results(search_text) if include_streaming
+    results = default_results_hash
+    movie = find_movie(search_text)
+    return results unless movie
+    results[:movie] = movie
+    results[:local] = local_results(movie['title'])
+    results[:streaming] = canistreamit_results(movie['_id']) if include_streaming && movie['_id']
     results
   end
 
-  def local_results(search_text)
-    local_movies = Movie.where('title like ?', "%#{search_text}%")
-    local_movies.any? ? local_movies.to_a : []
+  def find_movie(search_text)
+    # first try to find it on canistreamit
+    # if you cant find it there try to find it locally
+    movie = @client.search(search_text)
+    return movie[0] unless movie.empty?
+    Movie.where('lower(title) = ?', search_text.downcase).first
   end
 
-  def canistreamit_results(search_text)
-    Canistreamit::Client.new.search_and_query(search_text)
+  def local_results(search_text)
+    local_movies = Movie.where('lower(title) = ?', search_text.downcase)
+    local_movies.map(&:source)
+  end
+
+  def canistreamit_results(movie_id)
+    @client.query(movie_id, %w(streaming xfinity))
+  end
+
+  def canistreamit_movie(search_text)
+    @client.search(search_text)
   end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe SearchController, type: :controller do
   describe 'GET #search' do
     before do
       @movie = create(:movie)
-      @movie_prefix = create(:movie, title: 'maor 1')
-      @movie_prefix2 = create(:movie, title: 'maor 2')
+      @movie_prefix = create(:movie, title: 'maorleger', source: 'DVD')
+      @movie_prefix2 = create(:movie, title: 'maorleger', source: 'BLURAY')
     end
 
     it 'returns http success' do
@@ -13,27 +13,45 @@ RSpec.describe SearchController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
+    it 'works' do
+      get :search
+      expect(assigns[:results]).to have_key(:local)
+    end
+
+    context 'with streaming turned on' do
+      it 'works' do
+        get :search, include_streaming: true
+        expect(assigns[:results]).to have_key(:streaming)
+        expect(assigns[:results]).to have_key(:local)
+      end
+
+      it 'can search canistreamit' do
+        get :search, name: 'goodfellas', include_streaming: true
+        # TODO: fix this test once i decide on an interface for canistreamit
+        expect(assigns[:results][:movie]).to match(a_hash_including('title' => 'GoodFellas'))
+      end
+
+      it 'returns an empty hash if nothing is found' do
+        get :search, name: 'flkjdsfljdslkf', include_streaming: true
+        expect(assigns[:results][:streaming]).to eq([])
+      end
+    end
+
     it 'can search your local library' do
       get :search, name: @movie.title
-      expect(assigns(:results)).not_to be_nil
-      expect(assigns(:results)).to eq([@movie])
+      expect(assigns[:results][:local]).not_to be_nil
+      expect(assigns[:results][:local]).to eq(['DVD'])
     end
 
     it 'can return multiple movies by prefix' do
-      get :search, name: 'maor'
-      expect(assigns(:results)).not_to be_nil
-      expect(assigns(:results)).to eq([@movie_prefix, @movie_prefix2])
+      get :search, name: 'maorleger'
+      expect(assigns[:results][:local]).not_to be_nil
+      expect(assigns[:results][:local]).to eq([@movie_prefix.source, @movie_prefix2.source])
     end
 
     it 'returns an empty array if no movies are found' do
       get :search, name: 'fuzzywazzywashe'
-      expect(assigns[:results]).to eq([])
-    end
-
-    it 'can search canistreamit' do
-      get :search, name: 'goodfellas', include_streaming: true
-      # TODO: fix this test once i decide on an interface for canistreamit
-      expect(assigns[:results][0]['movie']).to include('title' => 'GoodFellas')
+      expect(assigns[:results][:local]).to eq([])
     end
   end
 end
